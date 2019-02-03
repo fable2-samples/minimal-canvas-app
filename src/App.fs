d4fd81f90a6c091460bfe7d975800f21129554da
Kristian Mandrup <kmandrup@gmail.com>
2019-02-03 17:52:54 +0100

    add .vscode folder and try to import working env config files from working Clock app

--- src/App.fs
+++ src/App.fs
@@ -11,7 +11,7 @@ let init() =
     let ctx = canvas.getContext_2d()
     ctx.fillStyle <- !^"rgb(200,0,0)"
     ctx.fillRect (10., 10., 55., 50.)
-    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
+    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"    
     ctx.fillRect (30., 30., 55., 50.)
 
 init()
